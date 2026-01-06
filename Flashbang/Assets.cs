3b669d6b6c787141b88de75935e61475b13297ca
mwoiii <147194899+mwoiii@users.noreply.github.com>
2026-01-06 14:50:34 +0000

    there are flashbang changes in my flashbang files. it looks like auto formatting, I will commit anyway
    King Burger

--- Flashbang/Assets.cs
+++ Flashbang/Assets.cs
@@ -1,65 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.IO;
+﻿using System.IO;
 using UnityEngine;
 
-namespace Flashbang
-{
-    public static class Assets
-    {
+namespace Flashbang {
+    public static class Assets {
 
         public static AssetBundle mainAssetBundle = null;
         internal static string assetBundleName = "flashbang";
-        internal static string assemblyDir
-        {
-            get
-            {
+        internal static string assemblyDir {
+            get {
                 return Path.GetDirectoryName(Flashbang.pluginInfo.Location);
             }
         }
 
-        public static void PopulateAssets()
-        {
+        public static void PopulateAssets() {
             mainAssetBundle = AssetBundle.LoadFromFile(Path.Combine(assemblyDir, assetBundleName));
         }
     }
 
-    internal static class SoundBanks
-    {
+    internal static class SoundBanks {
         private static bool initialized = false;
 
-        public static string SoundBankDirectory
-        {
-            get
-            {
+        public static string SoundBankDirectory {
+            get {
                 return Path.Combine(Assets.assemblyDir);
             }
         }
 
-        public static void Init()
-        {
+        public static void Init() {
             if (initialized) return;
             initialized = true;
             AKRESULT akResult = AkSoundEngine.AddBasePath(SoundBankDirectory);
-            if (akResult == AKRESULT.AK_Success)
-            {
+            if (akResult == AKRESULT.AK_Success) {
                 Log.Info($"Added bank base path : {SoundBankDirectory}");
-            }
-            else
-            {
+            } else {
                 Log.Error(
                     $"Error adding base path : {SoundBankDirectory} " +
                     $"Error code : {akResult}");
             }
 
             AkSoundEngine.LoadBank("flashbang.bnk", out _);
-            if (akResult == AKRESULT.AK_Success)
-            {
+            if (akResult == AKRESULT.AK_Success) {
                 Log.Info($"Added bank : {"flashbang.bnk"}");
-            }
-            else
-            {
+            } else {
                 Log.Error(
                     $"Error loading bank : {"flashbang.bnk"} " +
                     $"Error code : {akResult}");
--- Flashbang/Flashbang.cs
+++ Flashbang/Flashbang.cs
@@ -1,29 +1,15 @@
+using System;
+using System.Collections;
 using BepInEx;
 using R2API;
+using R2API.Networking;
+using R2API.Networking.Interfaces;
 using RoR2;
-using RoR2.Stats;
-using System.IO;
-using System;
-using System.Text;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using IL.RoR2.UI;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 using UnityEngine.Networking;
-using R2API.Networking.Interfaces;
-using R2API.Networking;
-using RoR2.Networking;
-using UnityEngine.UI;
-using EntityStates;
-using Facepunch.Steamworks;
-using static System.Net.Mime.MediaTypeNames;
 
-namespace Flashbang
-{
+namespace Flashbang {
     [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
 
     [BepInDependency(ItemAPI.PluginGUID)]
@@ -32,8 +18,7 @@ namespace Flashbang
 
     [BepInDependency(LanguageAPI.PluginGUID)]
 
-    public class Flashbang : BaseUnityPlugin
-    {
+    public class Flashbang : BaseUnityPlugin {
         public const string PluginGUID = PluginAuthor + "." + PluginName;
         public const string PluginAuthor = "bouncyshield";
         public const string PluginName = "Flashbang";
@@ -43,8 +28,7 @@ namespace Flashbang
 
         public static Flashbang Instance;
 
-        public void Awake()
-        {
+        public void Awake() {
             pluginInfo = Info;
             Log.Init(Logger);
             Assets.PopulateAssets();
@@ -58,15 +42,13 @@ namespace Flashbang
             NetworkingAPI.RegisterMessageType<Sink>();
         }
 
-        public void OnDestroy()
-        {
+        public void OnDestroy() {
             On.RoR2.UI.HUD.Awake -= GetHud;
             On.RoR2.EquipmentSlot.PerformEquipmentAction -= EquipmentActivate;
         }
 
         private RoR2.UI.HUD hud;
-        private void GetHud(On.RoR2.UI.HUD.orig_Awake orig, RoR2.UI.HUD self)
-        {
+        private void GetHud(On.RoR2.UI.HUD.orig_Awake orig, RoR2.UI.HUD self) {
             orig(self);
             SoundBanks.Init(); // fancy meeting you here
             hud = self;
@@ -97,16 +79,13 @@ namespace Flashbang
 
         private GameObject Whitescreen;
         private GameObject Dizzyscreen;
-        private void FlashAlpha(float x)
-        {
+        private void FlashAlpha(float x) {
             Whitescreen.GetComponent<UnityEngine.UI.Image>().color = new UnityEngine.Color(1, 1, 1, x);
             Dizzyscreen.GetComponent<UnityEngine.UI.Image>().color = new UnityEngine.Color(1, 1, 1, x);
         }
 
-        private void FlashbangPlayer()
-        {
-            if (Whitescreen != null && Dizzyscreen != null)
-            {
+        private void FlashbangPlayer() {
+            if (Whitescreen != null && Dizzyscreen != null) {
                 Texture2D buh = ScreenCapture.CaptureScreenshotAsTexture();
                 Dizzyscreen.GetComponent<UnityEngine.UI.Image>().sprite = Sprite.Create(buh, new Rect(0, 0, buh.width, buh.height), Vector2.zero);
 
@@ -115,26 +94,21 @@ namespace Flashbang
                 AkSoundEngine.PostEvent(2753768932, Run.instance.gameObject);
 
                 Log.Info("Flashbang!");
-            }
-            else
-            {
+            } else {
                 Log.Warning("Bad flashbang...");
             }
         }
 
-        private void BangPlayer(NetworkInstanceId sourceId)
-        {
+        private void BangPlayer(NetworkInstanceId sourceId) {
             CharacterMaster source = CharacterMaster.instancesList.Find((CharacterMaster x) => x.networkIdentity.netId == sourceId);
             AkSoundEngine.PostEvent(2315197877, source.GetBody().gameObject);
         }
 
         private float alpha;
-        private IEnumerator Fade()
-        {
+        private IEnumerator Fade() {
             alpha = 1;
             FlashAlpha(alpha);
-            while (alpha >= 0)
-            {
+            while (alpha >= 0) {
                 yield return new WaitForSeconds(0.04f);
                 alpha -= 0.01f * (2 - alpha);
                 FlashAlpha(alpha);
@@ -143,8 +117,7 @@ namespace Flashbang
         }
 
         private EquipmentDef flashbang;
-        private void CreateEquipment()
-        {
+        private void CreateEquipment() {
             flashbang = ScriptableObject.CreateInstance<EquipmentDef>();
 
             flashbang.name = "FLASHBANG_NAME";
@@ -166,37 +139,26 @@ namespace Flashbang
             ItemAPI.Add(new CustomEquipment(flashbang, new ItemDisplayRuleDict(null)));
         }
 
-        private bool EquipmentActivate(On.RoR2.EquipmentSlot.orig_PerformEquipmentAction orig, EquipmentSlot self, EquipmentDef def)
-        {
-            if (def == flashbang && NetworkServer.active)
-            {
+        private bool EquipmentActivate(On.RoR2.EquipmentSlot.orig_PerformEquipmentAction orig, EquipmentSlot self, EquipmentDef def) {
+            if (def == flashbang && NetworkServer.active) {
                 FireFlashbang(self.characterBody);
                 return true;
-            }
-            else
-            {
+            } else {
                 return orig(self, def);
             }
         }
 
-        private void FireFlashbang(CharacterBody body)
-        {
+        private void FireFlashbang(CharacterBody body) {
             Vector3 origin = body.gameObject.transform.position;
-            foreach (CharacterBody x in CharacterBody.instancesList)
-            {
+            foreach (CharacterBody x in CharacterBody.instancesList) {
                 double distance = Vector3.Distance(x.gameObject.transform.position, origin);
                 // Players
-                if (x.isPlayerControlled)
-                {
+                if (x.isPlayerControlled) {
                     NetMessageExtensions.Send(new Sink(x.master.networkIdentity.netId, body.master.networkIdentity.netId, distance <= 80), (NetworkDestination)1);
-                }
-                else if (distance <= 80)
-                {
+                } else if (distance <= 80) {
                     // Enemies
-                    if (x.teamComponent.teamIndex != body.teamComponent.teamIndex)
-                    {
-                        DamageInfo boop = new()
-                        {
+                    if (x.teamComponent.teamIndex != body.teamComponent.teamIndex) {
+                        DamageInfo boop = new() {
                             damage = Math.Max(x.healthComponent.fullCombinedHealth / 10, body.baseDamage * 1.5f),
                             //inflictor = body.gameObject,
                             attacker = body.gameObject,
@@ -208,54 +170,44 @@ namespace Flashbang
                         SetStateOnHurt.SetStunOnObject(x.gameObject, 5);
                     }
                     // Allies
-                    else
-                    {
+                    else {
                         SetStateOnHurt.SetStunOnObject(x.gameObject, 5);
                     }
                 }
             }
         }
 
-        
-        public class Sink : INetMessage, ISerializableObject
-        {
+
+        public class Sink : INetMessage, ISerializableObject {
             private NetworkInstanceId playerId;
             private NetworkInstanceId sourceId;
             private bool withinRange;
 
             public Sink() { }
 
-            public Sink(NetworkInstanceId playerId, NetworkInstanceId sourceId, bool withinRange)
-            {
+            public Sink(NetworkInstanceId playerId, NetworkInstanceId sourceId, bool withinRange) {
                 this.playerId = playerId;
                 this.sourceId = sourceId;
                 this.withinRange = withinRange;
             }
 
-            public void Serialize(NetworkWriter writer)
-            {
+            public void Serialize(NetworkWriter writer) {
                 writer.Write(playerId);
                 writer.Write(sourceId);
                 writer.Write(withinRange);
             }
 
-            public void Deserialize(NetworkReader reader)
-            {
+            public void Deserialize(NetworkReader reader) {
                 playerId = reader.ReadNetworkId();
                 sourceId = reader.ReadNetworkId();
                 withinRange = reader.ReadBoolean();
             }
 
-            public void OnReceived()
-            {
-                if (playerId == LocalUserManager.GetFirstLocalUser().cachedMaster.networkIdentity.netId)
-                {
-                    if (withinRange)
-                    {
+            public void OnReceived() {
+                if (playerId == LocalUserManager.GetFirstLocalUser().cachedMaster.networkIdentity.netId) {
+                    if (withinRange) {
                         Instance.FlashbangPlayer();
-                    }
-                    else
-                    {
+                    } else {
                         Instance.BangPlayer(sourceId); // hahaha
                     }
                 }
